77cdfe886fccaa04c100d93e5f9941e63a99b9b3
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-08 13:04:01 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number1 = 3;
+        int number2 = 5;
+        int result = number1 + number2;
+
+        System.out.println(number1 + "" + number2);
+
+    }
+}
